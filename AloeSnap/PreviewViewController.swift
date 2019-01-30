23b12d76e448ae63bb4dc5cf11c30e94c5d4396f
Stephanie Chiu <stephaniechiu@outlook.com>
2019-01-30 07:12:55 -0800

    Build custom camera layout and functions

---
+++ AloeSnap/PreviewViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ImagePreviewViewController.swift
+//  AloeSnap
+//
+//  Created by Stephanie on 1/29/19.
+//  Copyright © 2019 Stephanie Chiu. All rights reserved.
+//
+
+import UIKit
+
+class PreviewViewController: UIStackView {
+
+    @IBAction func cancelImageButton(_ sender: Any) {
+    }
+    @IBAction func saveImageButton(_ sender: Any) {
+    }
+    
+    
+}
--- AloeSnap/ViewController.swift
+++ AloeSnap/ViewController.swift
@@ -2,19 +2,59 @@
 //  ViewController.swift
 //  AloeSnap
 //
-//  Created by Stephanie on 1/28/19.
+//  Created by Stephanie Chiu on 1/28/19.
 //  Copyright © 2019 Stephanie Chiu. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var captureSession = AVCaptureSession()
+    var backCamera: AVCaptureDevice?
+    var frontCamera: AVCaptureDevice?
+    var currentCamera: AVCaptureDevice?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        setupCaptureSession()
+        setupDevice()
+        setupInputOutput()
+        setupPreviewLayer()
+        startRunningCaptureSession()
 
+    }
+    
+    //Specify image quality we want. This will preset photo in full resolution
+    func setupCaptureSession() {
+        
+    }
+    
+    //Finds and monitors camera
+    func setupDevice() {
+
+    }
+    
+    func setupInputOutput() {
+        
+    }
+    
+    
+    func setupPreviewLayer() {
+        
+    }
+    
+    func startRunningCaptureSession() {
+        
+    }
+    
+    @IBAction func cameraButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "photoPreviewSegue", sender: nil)
+    }
+    
 
 }
 
